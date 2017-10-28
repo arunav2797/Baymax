307c7719e5ef647957b4ad634069cb92aa284b61
rishabh19038 <rishabh19038@yahoo.in>
2017-10-28 21:14:05 +0530

    Initial Commit

---
+++ Baymax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Baymax
+//
+//  Created by Rishabh Mittal on 28/10/17.
+//  Copyright © 2017 Rishabh Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BaymaxTests/BaymaxTests.swift
@@ -0,0 +1,36 @@
+//
+//  BaymaxTests.swift
+//  BaymaxTests
+//
+//  Created by Rishabh Mittal on 28/10/17.
+//  Copyright © 2017 Rishabh Mittal. All rights reserved.
+//
+
+import XCTest
+@testable import Baymax
+
+class BaymaxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
